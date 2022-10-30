a0b0208444952590123220c8565d9075215f013c
jadna30 <115428268+Jadna-Dev@users.noreply.github.com>
2022-10-30 17:17:28 +0200

    -m

--- Auto_Email.py
+++ Auto_Email.py
@@ -6,15 +6,17 @@ import smtplib
 #Step 1: Make Sure You Activate 2 Factor Auth On Your Google Account
 #Step 2: Go To /apppaswords On Your Google Account Dashboard
 #Step 3: Create A New App And Save Your Password Somewhere In Your Pc
-#Step 4: Open The Text File From The Location You Saved It like Showen Bellow 
+#Step 4: Open The Text File From The Location You Saved It like Shown Bellow 
 #Step 5: Setup Is Complete Follow Along And Enjoy
+
+#👇 Text File Containing Your Google App Password
 with open("C:\\Users\\jadna\\Desktop\\Youtube\\ps.txt","r") as ps1:
-    ps = ps1.readlines()
+    ps = ps1.read()
     
 #Initialze Your Variables     
 email_sender = "jadnahle99@gmail.com"
-email_password = ps[0]
-email_reciver = "jadnadev@gmail.com"
+email_password = ps
+email_reciver = ""
 
 #Set Your Email Content
 subject = "Automated Email"
@@ -31,7 +33,7 @@ em.set_content(body)
 #SSL Is A Security Protocol
 context = ssl.create_default_context()
 
-#Connecting To Gmail Servers 
+#Connecting To Gmail Servers And Sending The Mail
 with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context)as smtp :
     smtp.login(email_sender,email_password)
     smtp.sendmail(email_sender, email_reciver, em.as_string())
